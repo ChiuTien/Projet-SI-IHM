e0bb043442b01a466966dd94769b47ac646d6960
VlexandreMG <dodolashmou@gmail.com>
2026-04-01 09:43:30 +0300

    changement de config.php et etat.txt

--- app/config/config.php
+++ app/config/config.php
@@ -66,11 +66,10 @@ return [
 	 *         Database Settings          *
 	 **************************************/
 	'database' => [
-		// MySQL Example:
-		// 'host'     => 'localhost',      // Database host (e.g., 'localhost', 'db.example.com')
-		// 'dbname'   => 'your_db_name',   // Database name (e.g., 'flightphp')
-		// 'user'     => 'your_username',  // Database user (e.g., 'root')
-		// 'password' => 'your_password',  // Database password (never commit real passwords)
+		'host'     => '127.0.0.1',      // Database host (e.g., 'localhost', 'db.example.com')
+		'dbname'   => 'election',   // Database name (e.g., 'flightphp')
+		'user'     => 'root',  // Database user (e.g., 'root')
+		'password' => 'root',  // Database password (never commit real passwords)
 
 		// SQLite Example:
 		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite', // Path to SQLite file
