c52c31aae748719e1487ca0ac98922bb0abb961b
VlexandreMG <dodolashmou@gmail.com>
2026-04-01 11:31:14 +0300

    view statique beuuh

---
+++ app/views/result.php
@@ -0,0 +1,15 @@
+<?php 
+<h2>Résultat</h2>
+<table>
+    <tr>
+        <th>Candidat</th>
+        <th>Nombre d'électeurs</th>
+    </tr>
+    <tr>
+        <td><?= $candidat['nom'] ?></td>
+        <td><?= $nbElecteurs ?></td>
+    </tr>
+</table>
+
+?>
+
